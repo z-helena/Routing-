f2c4bb67c1bd07310c36a21467c863ce8432daa9
yzsythsbt <helena_zhang@horacemann.org>
2025-02-12 13:50:17 -0500

    add audio to route

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/boba-quiz">Boba Quiz</RouterLink>
+        <RouterLink to="/audio">Audio Quiz</RouterLink>
       </nav>
     </div>
   </header>
